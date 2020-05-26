186e4a074f2cc0ca6f9626e201c28e5636ac9d89
El Diablo <eldiablolives@live.com>
2020-05-26 07:27:07 +0700

    error fix and logging

--- src/handlers/v1/delegate/activate.ts
+++ src/handlers/v1/delegate/activate.ts
@@ -14,7 +14,6 @@ export default class DelegateActivationHandler {
   }
 
   async get(req, res, next) {
-    console.log(`!split --------> ${JSON.stringify(req.path.split('/'))} -> ${JSON.stringify(this._user)}`);
     try {
       return res.json(await this._api.activate(this._user.id, req.path.split('/')[4]));
     } catch (err) {
--- src/handlers/v1/delegates.ts
+++ src/handlers/v1/delegates.ts
@@ -3,17 +3,15 @@ import { Engine } from '@frogfish/kona';
 let logger;
 
 export default class DelegatesHandler {
-  private _util;
   constructor(private _engine: Engine, user) {
     logger = _engine.log.log('service:delegates');
-    this._util = require('@frogfish/kona/util');
   }
 
   async get(req, res, next) {
     try {
       return res.json(await this._engine.delegate.find(req.query, req.path.split('/')[3], req.path.split('/')[4]));
     } catch (err) {
-      this._util.error(err, res, logger, 'svc_delegates_get');
+      require('@frogfish/kona/util').error(err, res, logger, 'svc_delegates_get');
     }
   }
 }
